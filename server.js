df3f01ed00cd15879c0657c6ed47786dd558fecd
fernando <alvesfernandosantos1@gmail.com>
2018-05-25 08:11:19 -0300

    funcao para utilizar portas nao utilizadas no servidor(tirar a 3000)

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000; //nao manter porta 3000 no servidor
+//const port = 3000; //nao manter porta 3000 no servidor
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -22,3 +23,18 @@ app.use('/', route);
 
 server.listen(port);
 console.log('API rodando na porta ', port);
+
+
+function normalizePort(val){
+    const port = parseInt(val, 10);
+
+    if(isNaN(port)){ // se nao for um numero
+        return val;
+    }
+
+    if(port >= 0){
+        return port;
+    }
+
+    return false;
+}
