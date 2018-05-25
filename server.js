8785f4c24e9608fc6045fd473959146787ca80ff
fernando <alvesfernandosantos1@gmail.com>
2018-05-25 08:05:14 -0300

    criando uma api simples

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 'use strict'
 
-console.log('teste');
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = 3000; //nao manter porta 3000 no servidor
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "node api",
+        version: "0.0.1"
+    });
+});
+
+app.use('/', route);
+
+server.listen(port);
+console.log('API rodando na porta ', port);
