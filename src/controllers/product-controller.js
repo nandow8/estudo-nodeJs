390aa0cb822412849f58889cd7eae16e2d0c81f6
fernando <alvesfernandosantos1@gmail.com>
2018-05-26 09:39:40 -0300

    repositorio separando do controller(dao, aquelas coisas...)

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,51 +3,46 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
+const repository = require('../repositories/product-repository');
 
 exports.get = (req, res, next) => {
-    Product
-        .find({})
-        .then(data => {
-            res.status(200).send(data);
-        }).catch( e => {
-            res.status(400).send(e);
-        });
+    repository
+        .get()
+            .then(data => {
+                res.status(200).send(data);
+            }).catch( e => {
+                res.status(400).send(e);
+            });
 };
 
 exports.getBySlug = (req, res, next) => {
-    Product
-        .findOne({ //somente find eu trago um array, com find one eu trago corretamente pelo indice
-            slug: req.params.slug,
-            active:true
-        }, 'title description price slug tags')
-        .then(data => {
-            res.status(200).send(data);
-        }).catch( e => {
-            res.status(400).send(e);
-        });
+     repository
+         .getBySlug(req.params.slug)
+            .then(data => {
+                res.status(200).send(data);
+            }).catch( e => {
+                res.status(400).send(e);
+            });
 };
 
 exports.getById = (req, res, next) => {
-    Product
-        .findById( req.params.id )
-        .then(data => {
-            res.status(200).send(data);
-        }).catch( e => {
-            res.status(400).send(e);
-        });
+    repository
+        .getById(req.params.id)
+            .then(data => {
+                res.status(200).send(data);
+            }).catch( e => {
+                res.status(400).send(e);
+            });
 };
 
 exports.getByTag = (req, res, next) => {
-    Product
-        .find({
-            tags: req.params.tag,
-            active: true
-        }, 'title description price slug tags')
-        .then(data => {
-            res.status(200).send(data);
-        }).catch( e => {
-            res.status(400).send(e);
-        });
+    repository
+         .getByTag(req.params.tag)
+            .then(data => {
+                res.status(200).send(data);
+            }).catch( e => {
+                res.status(400).send(e);
+            });
 };
 
 exports.post = (req, res, next) => {
@@ -64,50 +59,44 @@ exports.post = (req, res, next) => {
 
     var product = new Product(req.body);
     //product.title = req.body.title;   posso passar um por um na hora de salvar
-    product
-       .save()
-       .then( x => {
-           res.status(201).send({ message: 'Produto salvo'});
-       }).catch( e => {
-           res.status(400).send({
-                message: 'Produto salvo',
-                data: e
-           });
-       });
+    repository
+         .create(req.body)
+            .then( x => {
+                res.status(201).send({ message: 'Produto salvo'});
+            }).catch( e => {
+                res.status(400).send({
+                        message: 'Produto salvo',
+                        data: e
+                });
+            });
 }
 
 exports.put = (req, res, next) => {
-    Product
-        .findByIdAndUpdate( req.params.id, {
-            $set: {
-                title: req.body.title,
-                description: req.body.description,
-                price: req.body.price,
-                slug: req.body.slug
-            }
-        }).then( x => {
-            res.status(200).send({
-                message: 'Produto atualizado com sucesso!'
+    repository
+        .update(req.params.id, req.body)
+            .then( x => {
+                res.status(200).send({
+                    message: 'Produto atualizado com sucesso!'
+                });
+            }).catch( e => {
+                res.status(400).send({
+                    message: 'Falha ao atualizar produto',
+                    data: e
+                });
             });
-        }).catch( e => {
-            res.status(400).send({
-                message: 'Falha ao atualizar produto',
-                data: e
-            });
-        });
 };
 
 exports.delete = (req, res, next) => {
-    Product
-        .findByIdAndRemove( req.params.id ) //ou posso passar body no lugar de params e nao excluir pela rota, mas pelo "post"
-        .then( x => {
-            res.status(200).send({
-                message: 'Produto removido com sucesso!'
+    repository
+        .delete(req.body.id) //ou posso passar body no lugar de params e nao excluir pela rota, mas pelo "post"
+            .then( x => {
+                res.status(200).send({
+                    message: 'Produto removido com sucesso!'
+                });
+            }).catch( e => {
+                res.status(400).send({
+                    message: 'Falha ao remover produto',
+                    data: e
+                });
             });
-        }).catch( e => {
-            res.status(400).send({
-                message: 'Falha ao remover produto',
-                data: e
-            });
-        });
 };
