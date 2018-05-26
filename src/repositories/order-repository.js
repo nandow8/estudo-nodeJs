613915529f791cd75f74bb88558122a672479a6f
fernando <alvesfernandosantos1@gmail.com>
2018-05-26 13:29:03 -0300

    listando dados com join LIMMMMDO

--- src/repositories/order-repository.js
+++ src/repositories/order-repository.js
@@ -4,7 +4,15 @@ const mongoose = require('mongoose');
 const Order = mongoose.model('Order');
 
 exports.get = async(data) => {
-    var res = await Order.find({});
+    //mostra TUDO
+    //var res = await Order.find({})
+    //.populate('customer') //traz tambem os dados do customer (JOIN T-T)
+    //.populate('items.product'); //é preciso dominar o array (seguir direitinho onde se encontra o produto)
+    
+    //mostra só o que eu quiser
+    var res = await Order.find({}, 'number status customer items')
+        .populate('customer', 'name') 
+        .populate('items.product', 'title');
     return res;
 }
 
