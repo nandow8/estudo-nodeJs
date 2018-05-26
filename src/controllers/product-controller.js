d67a1eef7514409244e80330fb6a2e429108f2c3
fernando <alvesfernandosantos1@gmail.com>
2018-05-26 09:16:38 -0300

    validacoes mais robustas(fluent validadtor git)

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 exports.get = (req, res, next) => {
     Product
@@ -50,6 +51,17 @@ exports.getByTag = (req, res, next) => {
 };
 
 exports.post = (req, res, next) => {
+    
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'A description deve conter pelo menos 3 caracteres');
+
+    if(!contract.isValid()){
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
     //product.title = req.body.title;   posso passar um por um na hora de salvar
     product
