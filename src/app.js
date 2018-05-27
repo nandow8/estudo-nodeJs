f0001886e788f53b6863ba5797b56c12b844d19e
fernando <alvesfernandosantos1@gmail.com>
2018-05-27 09:23:22 -0300

    limitando tamanho de arquivo e CORS

--- src/app.js
+++ src/app.js
@@ -18,8 +18,18 @@ const productsRoute = require('./routes/products-route');
 const customerRoute = require('./routes/customer-route');
 const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb' //tamanho do arquivo mandado para o servidor (aumentar caso tenha upload de imagem)
+}));
 app.use(bodyParser.urlencoded({ extended: false })); 
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
  
 app.use('/', indexRoute);
 app.use('/products', productsRoute); 
